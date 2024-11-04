a0bcbb494bc3dd1ed1e9877d55bd3439b4e2c1bf
Abdulwahab <abdulwahababdal@gmail.com>
2024-11-04 14:52:54 +0300

    done

--- iterationMethods.js
+++ iterationMethods.js
@@ -8,33 +8,70 @@ const transactions = [
 
 // 1) Write a `filterIncomeTransactions` function that returns a new array containing only the income transactions.
 function filterIncomeTransactions(transactions) {
-  // write your code here...
+  return transactions.filter((transaction) => transaction[0] === "income");
 }
 
 // 2) Write a `filterExpenseTransactions` function that return a new array containing only the expense transactions.
 function filterExpenseTransactions(transactions) {
-  // write your code here...
+  return transactions.filter((transaction) => transaction[0] === "expense");
 }
 
 // 3) Write a `calculateTotalIncome` function that calculates the total income and returns it.
 function calculateTotalIncome(transactions) {
-  // write your code here...
+  return transactions.reduce((total, transaction) => {
+    if (transaction[0] === "income") {
+      return total + transaction[1];
+    }
+    return total;
+  }, 0);
 }
 
 // 4) Write a `calculateTotalExpenses` function that calculates the total expense and returns it.
 function calculateTotalExpenses(transactions) {
-  // write your code here...
+  const expenseTransactions = transactions.filter((transaction) => {
+    return transaction[0] === "expense";
+  });
+  return expenseTransactions.reduce(function (total, transaction) {
+    return total + transaction[1];
+  }, 0);
 }
 
 // 5) Write a `calculateNetTotal` function that calculates the net total (total income - total expenses) and returns it.
 function calculateNetTotal(transactions) {
-  // write your code here...
+  const totalIncome = transactions
+    .filter((transaction) => {
+      return transaction[0] === "income";
+    })
+    .reduce((total, transaction) => {
+      return total + transaction[1];
+    }, 0);
+
+  const totalExpenses = transactions
+    .filter((transaction) => {
+      return transaction[0] === "expense";
+    })
+    .reduce((total, transaction) => {
+      return total + transaction[1];
+    }, 0);
+
+  return totalIncome - totalExpenses;
 }
 
 // 6) Write a `filterSignificantTransactions` function that identifies and creates a new array with transactions (both incomes and expenses) above or equal to $500.
 function filterSignificantTransactions(transactions, threshold = 500) {
-  // write your code here...
+  return transactions.filter((transaction) => {
+    return transaction[1] >= threshold;
+  });
 }
+
+module.exports = {
+  filterIncomeTransactions,
+  filterExpenseTransactions,
+  calculateTotalIncome,
+  calculateTotalExpenses,
+  calculateNetTotal,
+  filterSignificantTransactions,
+};
 
 module.exports = {
   filterIncomeTransactions,
